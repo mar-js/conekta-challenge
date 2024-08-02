c2628e80d42128ca85e64c1f931ffe0122a6715e
Martin <maguera37@gmail.com>
2024-08-02 18:27:08 -0300

    create tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["index.html", "src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { gridTemplateColumns: { 'auto': 'repeat(auto-fill, minmax(300px, 1fr))' } } },
+	plugins: [],
+};
